43ca246ed045cee003fc375a03295e3187096bb5
vsagar200 <Vikrant_Sagar@cable.comcast.com>
2016-03-31 13:59:27 -0400

    integrated telegram bot with console

--- nlpdemo_bot.py
+++ nlpdemo_bot.py
@@ -7,6 +7,7 @@ import json
 firebase = firebase.FirebaseApplication('https://chitchatbot.firebaseio.com', None)
 nlpbot_demo_url = "https://api.telegram.org/bot174911153:AAF2WXgU7GOo9_Lw2qkiREdpjbWciUZmqDY/sendMessage"
 
+
 def process(responses):
     if responses != None and type(responses) == type({}) and len(responses) > 0:
         print "============================"
@@ -14,13 +15,13 @@ def process(responses):
             data = responses[key]
             text = data["message"]["text"]
             chat_id = data["message"]["chat"]["id"]
+            print text
+            print chat_id
             out = console.process(text)
             payload = {
                 "chat_id": chat_id,
                 "text": json.dumps(out)
             }
-            print text
-            print chat_id
             res = requests.post(nlpbot_demo_url, data=payload)
             print res.text
             firebase.delete('/messages', key)
